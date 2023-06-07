a25f95e5d48d05a112ecf4b197d23ba95c314e06
Prashant Mohan <prashantmohan9598@gmail.com>
2023-06-08 01:18:39 +0530

    Implemented Nested Comments Using Recursion

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
+import CommentsContainer from './CommentsContainer';
 
 const WatchPage = () => {
     const [searchParams] = useSearchParams();
@@ -15,15 +16,19 @@ const WatchPage = () => {
                     //Works fine even if not added in dep array
   return (
     <div>
-        <iframe className='px-5'
-            width="1200" 
-            height="600" 
-            src={ "https://www.youtube.com/embed/" + searchParams.get("v") } 
-            title="YouTube video player" 
-            frameBorder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            allowFullScreen
-        ></iframe>
+        <div className='flex flex-col'>
+            <iframe className='px-5'
+                width="1200" 
+                height="600" 
+                src={ "https://www.youtube.com/embed/" + searchParams.get("v") } 
+                title="YouTube video player" 
+                frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowFullScreen
+            ></iframe>
+        </div>
+
+        <CommentsContainer />
     </div>
   )
 }
